8bc0f11f801423e1b1da0bf8af00f389cb35c56e
Emine Batar <btrusa45@gmail.com>
2020-08-17 00:03:00 -0500

    change

--- src/first_package/WelcomingTeam.java
+++ src/first_package/WelcomingTeam.java
@@ -15,8 +15,8 @@ public class WelcomingTeam {
 		System.out.println("Hi from Rahmi");
 		System.out.println("this is a trial");
 		System.out.println("Hi from Rahmi");
-		System.out.println("dont worry be happy");
-		System.out.println("dont worry be happy");
+		System.out.println("don't worry be happy");
+
 
 
 
